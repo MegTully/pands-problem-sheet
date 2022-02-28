7f4e93c57ddf3a042f2e485950bb890d48b12d42
MegTully <g00411235@gmit.ie>
2022-02-28 18:52:49 +0000

    pands-problem-sheet

---
+++ weekday.py
@@ -0,0 +1,9 @@
+import datetime
+#Today = datetime.datetime.now().isoweekday()
+
+#if Today < 6:
+#    print("Yes, unfortuneately today is a Weekday")
+#else:
+#    print ("It is the weekend, yay!")
+
+print(datetime.datetime.now().isoweekday()) #https://docs.python.org/3/library/datetime.html
