3ec0bcfcd09fc0745f1df70c878969c622ab4ebf
MegTully <g00411235@gmit.ie>
2022-03-11 23:14:44 +0000

    PANDS-PROBLEM-SHEET

---
+++ squareroot.py
@@ -0,0 +1,23 @@
+#Newtons Method -> root = Xn - (f(Xn)/ f'(Xn))  
+
+
+
+def sqrt():
+    a = (input("Please enter a number: "))
+    a= float #say sqrt 2
+    x = float
+    
+    while a > 0:
+        sq = x*x
+        (sq - a/2) = 0
+
+
+
+
+
+
+ 
+
+sqrt()
+    
+
